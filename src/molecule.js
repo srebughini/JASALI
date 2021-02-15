a4044356de77637c1e021420e969be820cce610e
Stefano Rebughini <ste.rebu@outlook.it>
2021-02-15 09:11:39 +0100

    Add other molecules

--- src/molecule.js
+++ src/molecule.js
@@ -2,24 +2,108 @@ import { AsaliError } from "./utils.js"
 import chemistryPkg from '@chemistry/elements';
 
 let moleculesDict = {
-    "AC3H4": {
-        elementsSymbol: ["C", "H"],
-        numberOfAtoms: [3, 4]
-    },
-    "H2": {
-        elementsSymbol: ["H"],
-        numberOfAtoms: [2]
-    }
+    "AR":
+        { elementSymbol: ["Ar"], numberOfAtoms: [1] },
+    "HE":
+        { elementSymbol: ["He"], numberOfAtoms: [1] },
+    "CH4":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [4, 1] },
+    "C3H8":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 3] },
+    "C3H6":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [6, 3] },
+    "C2H4":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [4, 2] },
+    "C2H2":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [2, 2] },
+    "O2":
+        { elementSymbol: ["O"], numberOfAtoms: [2] },
+    "N2":
+        { elementSymbol: ["N"], numberOfAtoms: [2] },
+    "CO":
+        { elementSymbol: ["C", "O"], numberOfAtoms: [1, 1] },
+    "CO2":
+        { elementSymbol: ["C", "O"], numberOfAtoms: [1, 2] },
+    "H2O":
+        { elementSymbol: ["H", "O"], numberOfAtoms: [2, 1] },
+    "H2O2":
+        { elementSymbol: ["H", "O"], numberOfAtoms: [2, 2] },
+    "NO2":
+        { elementSymbol: ["O", "N"], numberOfAtoms: [2, 1] },
+    "NO":
+        { elementSymbol: ["O", "N"], numberOfAtoms: [1, 1] },
+    "N2O":
+        { elementSymbol: ["O", "N"], numberOfAtoms: [1, 2] },
+    "NH3":
+        { elementSymbol: ["H", "N"], numberOfAtoms: [3, 1] },
+    "CH2O":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [2, 1, 1] },
+    "CH3OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [4, 1, 1] },
+    "C6H6":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [6, 6] },
+    "C7H8":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 7] },
+    "N2H4":
+        { elementSymbol: ["H", "N"], numberOfAtoms: [4, 2] },
+    "CH3COOH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [4, 2, 2] },
+    "C2H5OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [6, 2, 1] },
+    "CH3COCH3":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [6, 3, 1] },
+    "MEK":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [8, 4, 1] },
+    "NC4H8":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 4] },
+    "IC4H8":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 4] },
+    "IC4H10":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [10, 4] },
+    "NC4H10":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [10, 4] },
+    "N1C4H9OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [10, 4, 1] },
+    "N2C4H9OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [10, 4, 1] },
+    "TC4H9OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [10, 4, 1] },
+    "IC4H9OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [10, 4, 1] },
+    "CYC5H8":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 5] },
+    "C5H8":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 5] },
+    "C6H5OH":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [6, 6, 1] },
+    "C6H5CHO":
+        { elementSymbol: ["H", "C", "O"], numberOfAtoms: [6, 7, 1] },
+    "NC7H14":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [14, 7] },
+    "NC7H16":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [16, 7] },
+    "XYLENE":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [10, 8] },
+    "C6H5C2H3":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [8, 8] },
+    "IC8H18":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [18, 8] },
+    "NC10H22":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [22, 10] },
+    "BIPHENYL":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [10, 12] },
+    "H2":
+        { elementSymbol: ["H"], numberOfAtoms: [2] },
+    "C2H6":
+        { elementSymbol: ["H", "C"], numberOfAtoms: [6, 2] }
 }
 
 export default function MoleculeComposition(gasSpecieName) {
     let _moleculeDict = _checkSpecieName(gasSpecieName);
     let _numberOfElements = _moleculeDict.elementsSymbol.length;
 
-    function _checkSpecieName(gasSpecieName)
-    {
-        try
-        {
+    function _checkSpecieName(gasSpecieName) {
+        try {
             return moleculesDict[gasSpecieName];
         }
         catch (error) {
@@ -32,18 +116,15 @@ export default function MoleculeComposition(gasSpecieName) {
         return _moleculeDict.elementsSymbol.map(symbol => chemistryPkg.ChemElements.getBySymbol(symbol));
     }
 
-    function getElementsSymbol()
-    {
+    function getElementsSymbol() {
         return _moleculeDict.elementsSymbol;
     }
 
-    function getNumberOfAtoms()
-    {
+    function getNumberOfAtoms() {
         return _moleculeDict.numberOfAtoms;
     }
 
-    function getElementCounterDict()
-    {
+    function getElementCounterDict() {
         let _elementCounter = {}
         for (let i = 0; i < _numberOfElements; i++) {
             _elementCounter[_moleculeDict.elementsSymbol[i]] = _moleculeDict.numberOfAtoms[i]
@@ -51,5 +132,5 @@ export default function MoleculeComposition(gasSpecieName) {
         return _elementCounter;
     }
 
-    return { getElementsList, getElementsSymbol, getNumberOfAtoms,  getElementCounterDict}
+    return { getElementsList, getElementsSymbol, getNumberOfAtoms, getElementCounterDict }
 }

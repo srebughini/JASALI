191702d0eab1197e14576e6a3408a13340edb75c
srebughini <ste.rebu@gmail.com>
2026-05-13 11:53:48 +0200

    Fix bug on diffusion

--- src/jasali.js
+++ src/jasali.js
@@ -859,7 +859,7 @@ export function GasMixture({
                 for (let j = 0; j < _numberOfSpecies; j++) {
                     if (j != i) {
                         A = A + _moleFraction[j] * _speciesMolecularWeight[j];
-                        B = B + _moleFraction[j] / _diff[j][i];
+                        B = B + Math.max(1e-16,_moleFraction[j]) / _diff[j][i];
                     }
 
                 }
